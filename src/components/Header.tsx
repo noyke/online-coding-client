aec276b3a15b8561528af14913f2aaf8c626d44a
noyke <noy.keren22@gmail.com>
2023-12-26 02:20:39 +0200

    undo test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <HStack padding="8px" spacing="24px">
       <Image src={logo} boxSize="48px" />
-      <Heading>Code Review test</Heading>
+      <Heading>Code Review</Heading>
     </HStack>
   );
 };
