808fb24b3d605308eeb48711aefb110b676c3fce
noyke <noy.keren22@gmail.com>
2023-12-26 02:04:12 +0200

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <HStack padding="8px" spacing="24px">
       <Image src={logo} boxSize="48px" />
-      <Heading>Code Review</Heading>
+      <Heading>Code Review test</Heading>
     </HStack>
   );
 };
